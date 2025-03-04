597ca2d6c78ecc485bd2e94fdeabdfa21b640c0e
Prathm0025 <prathamesh@underpinservices.com>
2025-03-04 15:10:28 +0530

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SocketGateway } from './socket.gateway';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, SocketGateway],
+})
+export class AppModule {}
+
+// src/app.controller.ts
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
